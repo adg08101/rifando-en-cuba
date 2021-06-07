090af5b8f3b36f410000c92734d4d9c2662463c9
klol <adg08101@gmail.com>
2021-06-07 18:36:19 -0400

    Initial commit
    Starting PHP

--- login.php
+++ login.php
@@ -1,6 +1,11 @@
 <?php
     if (isset($_POST['user'], $_POST['pass']))
-        echo $_POST['user'], $_POST['pass'];
+        if ($_POST['user'] == 'SmartDrops' && $_POST['pass'] == 'SD123')
+            header('Location: main.php');
+        else {
+            header('Location: index.php');
+            die();
+        }
     else
         header('Location: index.php');
         die();
