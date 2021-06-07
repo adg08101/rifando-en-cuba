5a2c5556fcfcb259c3fa11f03846aac0ded12832
klol <adg08101@gmail.com>
2021-06-07 18:09:50 -0400

    Initial commit
    Starting PHP

--- index.php
+++ index.php
@@ -1,3 +1,34 @@
-<?php
-    echo time();
-    
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
+
+    <title>Signin Template for Bootstrap</title>
+
+    <link rel="canonical"
+          href="https://getbootstrap.com/docs/4.0/examples/sign-in/">
+
+    <!-- Bootstrap core CSS -->
+    <link href="../../dist/css/bootstrap.min.css" rel="stylesheet">
+
+    <!-- Custom styles for this template -->
+    <link href="signin.css" rel="stylesheet">
+</head>
+
+<body class="text-center">
+<form class="form-signin">
+    <label for="inputEmail" class="sr-only">User</label>
+    <input type="text" id="inputUser" class="form-control"
+           placeholder="Username" required autofocus>
+    <label for="inputPassword" class="sr-only">Password</label>
+    <input type="password" id="inputPassword" class="form-control"
+           placeholder="Password" required>
+    <button class="btn btn-lg btn-primary btn-block" type="submit">Sign in
+    </button>
+</form>
+</body>
+</html>
